f7ef1766dbf16799e508d317804d49421b2a1d39
Devesh Rawat <devesh@roadzen.io>
2023-11-28 00:13:21 +0530

    feat: add subscription logic

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 //! src/main.rs
 #![allow(non_snake_case)]
+use dotenv::dotenv;
 use newsLetter::configuration::get_configuration;
 use newsLetter::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let configuration = get_configuration().expect("Failed to read configuration."); // We have removed the hard-coded `8000` - it's now coming from our settings!
-    println!("{:?}", configuration);
+    dotenv().ok();
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connenction = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    let listener = TcpListener::bind(&address)?;
+    println!("Running on: {:?}", address);
+    run(listener, connenction)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,39 +1,43 @@
 //! tests/health_check.rs
 
 use newsLetter::configuration::get_configuration;
-use sqlx::{Connection, PgConnection};
+use sqlx::{Connection, PgConnection, PgPool};
 use std::net::TcpListener;
 use tokio;
 // `tokio::test` is the testing equivalent of `tokio::main`.
 // `cargo expand --test health_check` (<- name of the test file)
 
-fn spawn_app() -> String {
+async fn spawn_app() -> String {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = newsLetter::startup::run(listener).expect("Failed to bind address");
+
+    let server = newsLetter::startup::run(listener, connection).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
 #[tokio::test]
 async fn health_check_works() {
-    let address = spawn_app();
+    let address = spawn_app().await;
     let client = reqwest::Client::new();
     let response = client
         .get(format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
-
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    let app_address = spawn_app();
+    let app_address = spawn_app().await;
     let configuration = get_configuration().expect("Unable to fetch configurations!");
     let connection_string = configuration.database.connection_string();
-    let connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to Postgres");
     let client = reqwest::Client::new();
@@ -47,11 +51,17 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .await
         .expect("Failed to execute request.");
     assert_eq!(200, response.status().as_u16());
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
-    let app_address = spawn_app();
+    let app_address = spawn_app().await;
     let client = reqwest::Client::new();
 
     let test_cases = vec![
