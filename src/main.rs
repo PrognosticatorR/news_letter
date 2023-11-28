d2d7bf024680995e60a95b3a6b5a4850f5ae7a81
Devesh Rawat <devesh@roadzen.io>
2023-11-28 22:05:04 +0530

    add logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 //! src/main.rs
 #![allow(non_snake_case)]
 use dotenv::dotenv;
+use env_logger::Env;
 use newsLetter::configuration::get_configuration;
 use newsLetter::startup::run;
 use sqlx::PgPool;
@@ -9,12 +10,12 @@ use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connenction = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(&address)?;
-    println!("Running on: {:?}", address);
     run(listener, connenction)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,7 @@
 //! src/startup.rs
 
 use crate::routes::{health_check, subscribe};
-use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -9,6 +9,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(pool.clone())
